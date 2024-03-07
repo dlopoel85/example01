da0b25a53b8dcac3a96bea440b312d31df63f768
dlopoel85 <oszkar.leopold&smartymind.hu>
2024-03-07 19:17:27 +0100

    1,2 kész, 3 folyamatban

---
+++ src/HomeorkIf.java
@@ -0,0 +1,30 @@
+public class HomeorkIf {
+
+    public static void main(String[] args) {
+
+        char letter = '/';
+
+        int a = 9;
+        int b = 4;
+
+        if (letter == '+') {
+            int c = a + b;
+            System.out.println("a + b = " + c);
+        }
+        if (letter == '-') {
+            int c = a - b;
+            System.out.println("a - b = " + c);
+        }
+        if (letter == '*') {
+            int c = a * b;
+            System.out.println("a * b = " + c);
+        }
+        if (letter == '/') {
+            int c = a / b;
+            System.out.println("a / b = " + c);
+        }
+
+
+        System.out.println("-----------------------------");
+    }
+}
---
+++ src/HomeorkIfMethod.java
@@ -0,0 +1,38 @@
+public class HomeorkIfMethod {
+
+    public static void main(String[] args) {
+
+        char letter = '/';
+
+        int a = 9;
+        int b = 4;
+        int c = 0;
+
+
+        resultOfnumber(letter);
+
+        public static void resultOfnumber(char charof) {
+
+
+        if (letter == '+') {
+            c = a + b;
+
+        }
+        if (letter == '-') {
+             c = a - b;
+
+        }
+        if (letter == '*') {
+            c = a * b;
+
+        }
+        if (letter == '/') {
+            c = a / b;
+
+        }
+            System.out.println("Műveleti jel: " + letter + " eredmény: " + c);
+        }
+
+        System.out.println("-----------------------------");
+    }
+}
